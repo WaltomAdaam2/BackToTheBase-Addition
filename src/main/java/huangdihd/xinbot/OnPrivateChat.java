b1901037583e34a6973926a3d40773d13d2b2749
WaltomAdaam <waltomadaam@gmail.com>
2026-06-05 21:03:53 +0800

    Refactor pearl button interaction logging and movements

--- src/main/java/huangdihd/xinbot/OnPrivateChat.java
+++ src/main/java/huangdihd/xinbot/OnPrivateChat.java
@@ -9,7 +9,7 @@ import xin.bbtt.mcbot.event.EventHandler;
 import xin.bbtt.mcbot.event.Listener;
 import xin.bbtt.mcbot.events.PrivateChatEvent;
 import xin.bbtt.movement.Movement;
-import xin.bbtt.movements.LookAtMovement;
+
 import xin.bbtt.movements.PathMovement;
 import xin.bbtt.pathfinding.DStarLite;
 import xin.bbtt.pathfinding.Node;
@@ -320,21 +320,12 @@ public class OnPrivateChat implements Listener {
                     return;
                 }
 
-                BackToTheBase.INSTANCE.getLogger().info(
-                        "Looking at pearl button location {} for {}.",
-                        location.getNumber(),
-                        playerName
-                );
-                
-                MovementSync.INSTANCE.getMovementController().addMovement(new LookAtMovement(
-                        target.hitPosition()
-                ));
-                
                 MovementSync.INSTANCE.getMovementController().addMovement(new UseItemOnMovement(
                         target.position(),
-                        target.direction()
+                        target.direction(),
+                        target.hitPosition()
                 ));
-                BackToTheBase.INSTANCE.getLogger().info("Clicking pearl button location {} for {}.", location.getNumber(), playerName);
+                BackToTheBase.INSTANCE.getLogger().info("Looking at and clicking pearl button location {} for {}.", location.getNumber(), playerName);
                 
                 if (config.isReturnAfterUse()) {
                     MovementSync.INSTANCE.getMovementController().addMovement(new ReturnAfterUseMovement(playerName, config.getReturnLocation()));
