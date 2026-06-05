4cd3806850c2e68bbf96bc2c36a59b169e2213d1
WaltomAdaam <waltomadaam@gmail.com>
2026-06-05 21:03:07 +0800

    Update UseItemOnMovement.java

--- src/main/java/huangdihd/xinbot/UseItemOnMovement.java
+++ src/main/java/huangdihd/xinbot/UseItemOnMovement.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 import org.cloudburstmc.math.vector.Vector3i;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.object.Direction;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.player.Hand;
+import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundMovePlayerPosRotPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundSwingPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundUseItemOnPacket;
+import org.joml.Vector3d;
+import xin.bbtt.MovementSync;
 import xin.bbtt.mcbot.Bot;
 import xin.bbtt.movement.Movement;
 
@@ -17,13 +20,42 @@ public class UseItemOnMovement extends Movement {
     @Getter
     private final Direction direction;
 
+    private final Vector3d lookTarget;
+    private int ticks = 0;
+    private boolean used = false;
+
     public UseItemOnMovement(Vector3i target, Direction direction) {
+        this(target, direction, null);
+    }
+
+    public UseItemOnMovement(Vector3i target, Direction direction, Vector3d lookTarget) {
         this.target = target;
         this.direction = direction;
+        this.lookTarget = lookTarget;
     }
 
     @Override
     public void init() {
+        if (lookTarget != null) {
+            sendLookPacket(lookTarget);
+        }
+    }
+
+    @Override
+    public void onTick() {
+        if (used) {
+            setFinished(true);
+            return;
+        }
+
+        ticks++;
+
+        // Wait about one movement tick after sending the rotation packet.
+        // This gives the server time to receive the new yaw/pitch before the right click packet.
+        if (ticks < 2) {
+            return;
+        }
+
         Bot.INSTANCE.getSession().send(new ServerboundUseItemOnPacket(
                 this.target,
                 this.direction,
@@ -38,6 +70,33 @@ public class UseItemOnMovement extends Movement {
 
         Bot.INSTANCE.getSession().send(new ServerboundSwingPacket(
                 Hand.MAIN_HAND
+        ));
+
+        used = true;
+        setFinished(true);
+    }
+
+    private void sendLookPacket(Vector3d target) {
+        Vector3d headPos = MovementSync.INSTANCE.getHeadPosition();
+        Vector3d diff = new Vector3d(target).sub(headPos);
+
+        double distanceXZ = Math.sqrt(diff.x * diff.x + diff.z * diff.z);
+        float targetYaw = (float) Math.toDegrees(Math.atan2(-diff.x, diff.z));
+        float targetPitch = (float) Math.toDegrees(Math.atan2(-diff.y, distanceXZ));
+
+        MovementSync.INSTANCE.yaw.set(targetYaw);
+        MovementSync.INSTANCE.pitch.set(targetPitch);
+
+        Vector3d pos = MovementSync.INSTANCE.position.get();
+
+        Bot.INSTANCE.getSession().send(new ServerboundMovePlayerPosRotPacket(
+                MovementSync.INSTANCE.onGround.get(),
+                false,
+                pos.x,
+                pos.y,
+                pos.z,
+                targetYaw,
+                targetPitch
         ));
     }
 
@@ -63,5 +122,14 @@ public class UseItemOnMovement extends Movement {
             case SOUTH -> 1.0f;
             default -> 0.5f;
         };
+    }
+
+    @Override
+    public long getTime() {
+        return 1000;
+    }
+
+    @Override
+    public void onStop() {
     }
 }
